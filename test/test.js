7b5bf57db744a66181c2d8fd6743909e7386177b
John Granström <john@soundation.com>
2020-02-14 10:20:49 +0100

    Add support for remote id

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var RedisPubSub = require('../index');
 
 require('@soundation/sharedb/test/pubsub')(function(callback) {
-  callback(null, RedisPubSub());
+  const rps = RedisPubSub()
+  rps.client.del('idseq:id');
+  callback(null, rps);
 });
